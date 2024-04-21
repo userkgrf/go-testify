7b462514fb7e7303f9e55ce31f0b26f3eff372a8
user2 <userkgrf>
2024-04-21 20:14:11 +0300

    second commit

--- precode.go
+++ precode.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "net/http"
-    "strconv"
-    "strings"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 var cafeList = map[string][]string{
--- testify_test.go
+++ testify_test.go
@@ -5,22 +5,23 @@ import (
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 )
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count="+strconv.Itoa(totalCount+1)+"&city=moscow", nil) // здесь нужно создать запрос к сервису
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	// здесь нужно добавить необходимые проверки
-	require.Equal(t, http.StatusOK, responseRecorder.Code)           // Проверяем код ответа
-	assert.NotEmpty(t, responseRecorder.Body.String())               // Проверяем, что тело ответа не пустое
-	assert.Equal(t, totalCount, len(responseRecorder.Body.String())) //Сверяем количество кафе
-	assert.Equal(t, cafeList, responseRecorder.Body.String())        // Проверяем все доступные кафе
+	require.Equal(t, http.StatusOK, responseRecorder.Code) // Проверяем код ответа
+	expectedCafes := strings.Join(cafeList["moscow"], ",")
+	assert.Equal(t, expectedCafes, responseRecorder.Body.String()) // Проверяем все доступные кафе
 }
 
 func TestMainHandlerWhenCityIsWrong(t *testing.T) {
